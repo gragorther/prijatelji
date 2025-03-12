53cbba1270c425274a8b40488601108fa9b0ea1a
Gregor <gregor@gregtech.eu>
2025-03-12 19:47:30 +0100

    made the uhh database thing, doesnt work as of now

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,1 +1,25 @@
-export async function load() {}
+import db from '$lib/db';
+
+export const actions = {
+	addFriend: async (event) => {
+		const form = await event.request.formData();
+		const name = form.get('name');
+		const age = form.get('age');
+		const interest = form.get('interest');
+
+    // Dodaj novega prijatelja
+    await db.friend.create({data: {
+      name: name
+      age: parseInt(age)
+      interest: interest
+    }})
+	}
+};
+
+export async function load() {
+	const friends = await db.friend.findMany();
+
+	return {
+		friends: friends
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,35 @@
 <script lang="ts">
+	export let data;
+	const friends = data.friends;
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello World</h1>
+<h1 class="text-3xl font-bold underline text-center">Friends</h1>
+
+<form
+	class="bg-slate-300 flex flex-col gap-4 p-4 border-purple-700 border rounded-md m-2 place-content-center"
+	method="POST"
+	action="?/addFriend"
+>
+	<input name="name" required />
+	<input name="age" type="number" required />
+	<select name="interest" required>
+		<option value="">Select interest</option>
+		<option value="sport">Sport</option>
+		<option value="music">Music</option>
+		<option value="programming">Programming</option>
+		<option value="sirov burek">Sirov burek</option>
+		<option value="mesni burek">Mesni burek</option>
+	</select>
+	<button type="submit" class="bg-purple-400 border border-purple-800 rounded-lg"
+		>Dodaj prijatelja</button
+	>
+</form>
+
+<div class="grid grid-cols-3 p-4 bg-slate-300 gap-4">
+	{#each friends as f}
+		<div class="bg-stone-400 rounded-md">
+			<div class="text-lg font-bold p-1">#{f.id} {f.name} ({f.age})</div>
+			<div class="p-1">{f.interest}</div>
+		</div>
+	{/each}
+</div>
