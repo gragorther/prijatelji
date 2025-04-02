bff32fddd1539ae9b26e447235accc9bfa7d0ace
Gregor <gregor@gregtech.eu>
2025-04-02 18:51:27 +0200

    more

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,15 +3,19 @@ import { fail } from '@sveltejs/kit';
 
 export const actions = {
 	addFriend: async (event) => {
+		console.log('kebab');
 		const form = await event.request.formData();
 		const name = form.get('name');
-		const age = form.get('age');
+		const age = parseInt(form.get('age'));
 		const interest = form.get('interest');
+		const favcolor = form.get('favcolor');
+		const image = form.get('image');
 
 		if (typeof name !== 'string') {
 			return fail(500, { name, incorrect: true });
 		}
 
+		console.log(typeof age);
 		if (typeof age !== 'number') {
 			return fail(500, { age, incorrect: true });
 		}
@@ -20,13 +24,30 @@ export const actions = {
 			return fail(500, { interest, incorrect: true });
 		}
 
+		if (typeof favcolor !== 'string') {
+			return fail(500, { favcolor, incorrect: true });
+		}
+		if (typeof image !== 'string') {
+			return fail(500, { image, incorrect: true });
+		}
+
 		// Dodaj novega prijatelja
 		await db.friend.create({
 			data: {
 				name: name,
-				age: parseInt(age),
-				interest: interest
+				age: age,
+				interest: interest,
+				color: favcolor,
+				image: image
 			}
+		});
+		console.log(name, age, interest, favcolor);
+	},
+	removeFriend: async (event) => {
+		const form = await event.request.formData();
+		const friend = form.get('friend');
+		await db.friend.delete({
+			where: { id: Number(friend) }
 		});
 	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
+	import { User } from 'lucide-svelte';
 	export let data;
 	const friends = data.friends;
+
+	let selectedFile: File | null = null;
+	let base64Image: string | null = null;
+
+	async function handleFileChange(event: Event) {
+		const fileInput = event.target as HTMLInputElement;
+		if (fileInput.files && fileInput.files.length > 0) {
+			selectedFile = fileInput.files[0];
+			base64Image = await fileToBase64(selectedFile);
+		}
+	}
+
+	function fileToBase64(file: File): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => resolve(reader.result as string);
+			reader.onerror = (error) => reject(error);
+		});
+	}
 </script>
 
 <h1 class="text-3xl font-bold underline text-center">Friends</h1>
@@ -26,17 +47,40 @@
 		<option value="sirov burek">Sirov burek</option>
 		<option value="mesni burek">Mesni burek</option>
 	</select>
+	<label for="favcolor">Select color:</label>
+	<input type="color" id="favcolor" name="favcolor" value="#ff0000" required />
+	<input type="file" on:change={handleFileChange} />
+	<input type="hidden" value={base64Image} name="image" />
 	<button type="submit" class="bg-purple-400 border border-purple-800 rounded-lg"
 		>Dodaj prijatelja</button
+	>
+</form>
+<form
+	class="bg-slate-300 flex flex-col gap-4 p-4 border-purple-700 border rounded-md m-2 place-content-center"
+	method="POST"
+	action="?/removeFriend"
+>
+	<select name="friend">
+		{#each friends as friend}
+			<option value={friend.id}>{friend.name}</option>
+		{/each}
+	</select>
+	<button type="submit" class="bg-purple-400 border border-purple-800 rounded-lg"
+		>Odstrani prijatelja</button
 	>
 </form>
 
 <div class="grid grid-cols-3 p-4 bg-slate-300 gap-4">
 	{#each friends as f}
-		<div class="bg-stone-400 rounded-md">
+		<div class="bg-stone-400 rounded-md" style="background-color:{f.color}">
 			<div class="text-lg font-bold p-1">#{f.id} {f.name} ({f.age})</div>
 			<div class="p-1">{f.interest}</div>
 			<div class="p-1">Created at: {f.timestamp}</div>
+			{#if f.image}
+				<img src={f.image} alt={f.name} />
+			{:else}
+				<User size="10rem" />
+			{/if}
 		</div>
 	{/each}
 </div>
